17b3f4c1eaae51c227c757f018d459b43429a799
OliBomby <olivierschipper@gmail.com>
2021-04-14 17:15:26 +0200

    Update BeatmapHelperTests.cs

--- Mapping_Tools_Core_Tests/BeatmapHelper/BeatmapHelperTests.cs
+++ Mapping_Tools_Core_Tests/BeatmapHelper/BeatmapHelperTests.cs
@@ -2,8 +2,8 @@
 using Mapping_Tools_Core.BeatmapHelper.Decoding;
 using Mapping_Tools_Core.BeatmapHelper.Encoding;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
-using System.Linq;
 
 namespace Mapping_Tools_Core_Tests.BeatmapHelper {
     [TestClass]
@@ -31,7 +31,15 @@ namespace Mapping_Tools_Core_Tests.BeatmapHelper {
         private static void TestUnchanging(string lines, IDecoder<Beatmap> decoder, IEncoder<Beatmap> encoder) {
             var lines2 = encoder.Encode(decoder.DecodeNew(lines));
 
-            Assert.AreEqual(lines, lines2);
+            // Split equal asserting to lines so we know where the difference is
+            var linesSplit = lines.Split(Environment.NewLine);
+            var lines2Split = lines2.Split(Environment.NewLine);
+
+            Assert.AreEqual(linesSplit.Length, lines2Split.Length);
+
+            for (int i = 0; i < linesSplit.Length; i++) {
+                Assert.AreEqual(linesSplit[i], lines2Split[i], $"Line equality fail at line {i+1}!");
+            }
         }
     }
 }
