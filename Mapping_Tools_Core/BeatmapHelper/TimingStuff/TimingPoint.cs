b3ff948baf1e582a925f85b7a5de7d51602820f3
OliBomby <olivierschipper@gmail.com>
2022-10-18 11:30:30 +0200

    Update TimingPoint.cs

--- Mapping_Tools_Core/BeatmapHelper/TimingStuff/TimingPoint.cs
+++ Mapping_Tools_Core/BeatmapHelper/TimingStuff/TimingPoint.cs
@@ -200,7 +200,7 @@ namespace Mapping_Tools_Core.BeatmapHelper.TimingStuff {
         /// </summary>
         /// <returns></returns>
         public double GetSliderVelocity() {
-            return !Uninherited && Mpb < 0 ? -100 / Mpb : 1;
+            return !Uninherited && MpB < 0 ? -100 / MpB : 1;
         }
 
         /// <summary>
